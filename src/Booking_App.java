2cc9594917312297082b7baaf61b050223fdbd7b
romit581 <romitrrs05@gmail.com>
2026-03-09 10:12:23 +0530

    UC4:Room Search & Availability Check

--- src/Booking_App.java
+++ src/Booking_App.java
@@ -2,34 +2,19 @@
 public class Booking_App {
     public static void main(String[] args) {
 
-        // Print a welcome message to the console
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
-
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom  = new SuiteRoom();
 
         RoomInventory inventory = new RoomInventory();
         inventory.initializeInventory();
+        RoomSearchService searchService = new RoomSearchService();
 
         // Display room details and availability
-        System.out.println("Hotel Room Inventory Status");
-        System.out.println();
 
-        singleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + inventory.getRoomAvailability().get("Single Room"));
+        System.out.println("Room Search");
         System.out.println();
-
-        // Display Double Room details + availability
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + inventory.getRoomAvailability().get("Double Room"));
-        System.out.println();
-
-        // Display Suite Room details + availability
-        suiteRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + inventory.getRoomAvailability().get("Suite Room"));
-        System.out.println();
+        searchService.searchAvailableRooms(inventory, singleRoom, doubleRoom, suiteRoom);
 
 
     }
